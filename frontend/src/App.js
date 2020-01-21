547e9bc7c8f8089416085446c711a9f88e5581c4
victorhugo_araujo <victorhugo_araujo@hotmail.com>
2020-01-21 19:58:33 -0300

    Add and list dev, new components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,92 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required/>
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required/>
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/14045736?s=460&v=4" alt="Victor Hugo Araujo"/>
-              <div className="user-info">
-                <strong>Victor Hugo Araujo</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Front end Developer at Hostgator</p>
-            <a href="https://github.com/victorhugoaraujo">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/14045736?s=460&v=4" alt="Victor Hugo Araujo"/>
-              <div className="user-info">
-                <strong>Victor Hugo Araujo</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Front end Developer at Hostgator</p>
-            <a href="https://github.com/victorhugoaraujo">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/14045736?s=460&v=4" alt="Victor Hugo Araujo"/>
-              <div className="user-info">
-                <strong>Victor Hugo Araujo</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Front end Developer at Hostgator</p>
-            <a href="https://github.com/victorhugoaraujo">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/14045736?s=460&v=4" alt="Victor Hugo Araujo"/>
-              <div className="user-info">
-                <strong>Victor Hugo Araujo</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Front end Developer at Hostgator</p>
-            <a href="https://github.com/victorhugoaraujo">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
-
       </main>
     </div>
   );
