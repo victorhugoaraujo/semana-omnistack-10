e7f6d15f1a12310045fdd572ec8050e8f6080328
victorhugo_araujo <victorhugo_araujo@hotmail.com>
2020-01-26 12:47:46 -0300

    Add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack10:omnistack10@cluster0-cu5vj.mongodb.net/week10?retryWrites=true&w=majority',{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  'mongodb+srv://omnistack10:omnistack10@cluster0-cu5vj.mongodb.net/week10?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
+app.use(cors());
 // Para o express entender o formato json
 app.use(express.json());
 app.use(routes);
